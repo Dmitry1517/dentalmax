746d79c0c0681bb673afebef1a30cfd693af3dab
Dmitry1517 <postbox404@gmail.com>
2025-11-19 02:33:14 +1000

    second-commit

---
+++ src/entities/BgAndForm.vue
@@ -0,0 +1,60 @@
+<script setup>
+import Header from '@/entities/Header.vue'
+</script>
+
+<template>
+  <section class="form">
+    <Header />
+    <div class="promo">
+      <div class="baner">
+        <h3 class="baner__title">
+          Качественное и безопасное<br />
+          лечение зубов в ДентАлМакс
+        </h3>
+        <p class="baner__text">
+          Мы предлагаем полный спектр стоматологических<br />
+          услуг, от профилактики до сложного протезирования,<br />
+          с использованием передовых технологий и материалов.
+        </p>
+        <button class="baner__button button">Записаться на прием</button>
+      </div>
+      <div class="tooth">
+        <img src="/tooth.png" alt="tooth" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.form {
+  background-image: url('/bg-fon.png');
+}
+.promo {
+  display: flex;
+  align-items: center;
+  width: 1280px;
+  margin: 0 auto;
+  padding: 0px 70px;
+}
+.baner {
+  background: var(--bg-white);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  border-radius: 25px;
+  gap: 50px;
+}
+.baner__title {
+  font-size: 35px;
+  text-align: center;
+}
+.baner__text {
+  text-align: center;
+  font-size: 20px;
+}
+
+.tooth {
+  flex: 1;
+}
+</style>
---
+++ src/entities/Header.vue
@@ -0,0 +1,77 @@
+<script setup>
+import Logo from '@/shared/icons/Logo.vue'
+</script>
+
+<template>
+  <header class="header">
+    <div class="logo">
+      <Logo />
+      <span>ДентАлМакс</span>
+    </div>
+    <div class="nav">
+      <ul class="nav__list">
+        <li class="nav__item">Клиника</li>
+        <li class="nav__item">Услуги</li>
+        <li class="nav__item">Специалисты</li>
+        <li class="nav__item">Отзывы</li>
+      </ul>
+    </div>
+    <div class="login">
+      <button class="button button_notbg">Войти</button>
+      <button class="button">Регистрация</button>
+    </div>
+  </header>
+</template>
+
+<style>
+.header {
+  width: 1280px;
+  margin: 0 auto;
+  padding: 22px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.logo {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+.logo span {
+  font-size: 40px;
+  color: var(--text-color);
+}
+
+.nav__list {
+  display: flex;
+  align-items: center;
+  gap: 40px;
+  list-style-type: none;
+}
+.nav__item {
+  color: var(--text-color);
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.login {
+  display: flex;
+  align-items: center;
+}
+
+.button {
+  background: var(--bg-button);
+  border-radius: 10px;
+  padding: 15px 25px;
+  color: var(--text-color);
+  font-weight: bold;
+  font-size: 20px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
+
+.button_notbg {
+  background: none;
+}
+</style>
