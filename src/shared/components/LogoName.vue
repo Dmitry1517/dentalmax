bf80d5afd62454eaae2e2bf1fed3d00e7196a0fc
Dmitry1517 <94603546+Dmitry1517@users.noreply.github.com>
2025-11-19 01:23:48 +0000

    component-logo

---
+++ src/shared/components/LogoName.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Logo from '@/shared/icons/Logo.vue'
+const {color} = defineProps({
+    color: String
+})
+</script>
+
+<template>
+    <div class="logo">
+        <Logo :color="color" />
+        <span class="logo__name">ДентАлМакс</span>
+    </div>
+</template>
+
+<style scoped>
+.logo {
+    display: flex;
+    align-items: center;
+}
+
+.logo__name {
+    font-size: 30px;
+    color: #fff;
+}
+</style>
